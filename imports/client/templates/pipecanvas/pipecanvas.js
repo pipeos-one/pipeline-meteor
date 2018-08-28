913ef1290e4c589cf70c3eb1d4d0fce82d4b6e60
Loredana <loredana.cirstea@gmail.com>
2018-08-28 10:27:37 +0200

    Add multiple functions to the PipedContract
    We can have multiple graphs now for a single contract.
    Each graph is a function.
    
    - Solidity code output now builds the contract with all the functions
    - JS code output now builds the js code for all the functions
    - pipegram output is now an array of graph JSON objects

--- imports/client/templates/pipecanvas/pipecanvas.js
+++ imports/client/templates/pipecanvas/pipecanvas.js
@@ -1,10 +1,13 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
 import joint from '/imports/client/lib/external/joint.js';
 import '/imports/client/lib/external/joint.css';
-import solidity from '../pipecodepreview/codepatterns.js';
 
 import './pipecanvas.html';
 import './pipecanvas.css';
 import toJavascript from './toJs.js';
+import GraphsToSolidity from './toSolidity.js';
 
 Template.pipecanvas.onCreated(function() {
     let self = this;
@@ -12,27 +15,61 @@ Template.pipecanvas.onCreated(function() {
     self.pipecode = self.data.pipecode;
     self.pipegram = self.data.pipegram;
     self.pipejscode = self.data.pipejscode;
-
-    self.update = function() {
-        let pipegram = JSON.stringify(self.graph.toJSON());
-        let pipecode = toSolidity(self.graph);
-        let pipejscode = toJavascript(self.graph, inputs);
-        self.pipegram.set(pipegram);
-        self.pipecode.set(pipecode);
-        self.pipejscode.set(pipejscode);
-    }
+    self.graph_index = new ReactiveVar(0);
+    self.max_graph_index = new ReactiveVar(0);
 });
 
 Template.pipecanvas.events({
     'click #pipecopy': function() {
         $('.tab-pane.active textarea')[0].select();
         document.execCommand("copy");
+    },
+    'click #addGraph': function() {
+        let max_graph_index = Template.instance().max_graph_index.get();
+        Template.instance().max_graph_index.set(++max_graph_index);
+        Template.instance().graph_index.set(max_graph_index);
+        let inst = Template.instance();
+        setTimeout(function() {
+            addGraph(inst);
+        }, 500);
+    },
+    'click #pipecanvasnav li': function(event) {
+        let index = parseInt(this.name.slice(8)) - 1;
+        Template.instance().graph_index.set(index);
+    }
+});
+
+Template.pipecanvas.helpers({
+    tab: function() {
+        let max_graph_index = Template.instance().max_graph_index.get();
+        return [...Array(max_graph_index + 1).keys()].map(function(index) {
+            return {name: `function${index + 1}`};
+        });
     }
 });
 
 Template.pipecanvas.onRendered(function() {
     let self = this
+    self.papers = []
+    self.graphs = []
     self.nodes = []
+
+    self.update = function() {
+        let pipegram = JSON.stringify(
+            self.graphs.map(function(graph) {
+                return graph.toJSON();
+            })
+        );
+        let graphs_to_sol = new GraphsToSolidity(self.graphs);
+        let pipecode = graphs_to_sol.toSolidity();
+        let pipejscode = '';
+        graphs_to_sol.graphs.map(function(graph) {
+            pipejscode += toJavascript(graph.graph, graph.inputs) + '\n';
+        });
+        self.pipegram.set(pipegram);
+        self.pipecode.set(pipecode);
+        self.pipejscode.set(pipejscode);
+    }
 
     joint.dia.ElementView.prototype.pointerdown = function(evt, x, y) {
        var data = this.eventData(evt);
@@ -42,13 +79,37 @@ Template.pipecanvas.onRendered(function() {
         this.dragStart(evt, x, y);
     };
 
-    self.graph = new joint.dia.Graph;
-    self.paper = new joint.dia.Paper({
-        el: document.getElementById('paper'),
+    addGraph(self);
+
+    Tracker.autorun(function() {
+        let contract_function = self.functionAbi.get();
+        Tracker.nonreactive(function() {
+            let graph_index = self.graph_index.get();
+            console.log('add node graph_index', graph_index);
+            if (contract_function) {
+                if (!self.nodes[graph_index]) {
+                    self.nodes[graph_index] = [];
+                }
+                self.nodes[graph_index] = addNode(self.graphs[graph_index], self.nodes[graph_index], contract_function)
+            }
+        });
+    });
+
+    $($('#pipecanvasnav li')[0]).addClass('active');
+    $('#function1').addClass('active');
+
+});
+
+function addGraph(self) {
+    let index = self.papers.length;
+    console.log('addGraph', index);
+    self.graphs.push(new joint.dia.Graph);
+    paper = new joint.dia.Paper({
+        el: document.getElementById(`paper_function${index + 1}`),
         width: "100%",
         height: "100%",
         gridSize: 2,
-        model: self.graph,
+        model: self.graphs[index],
         snapLinks: true,
         defaultLink: new joint.dia.Link({
             router: { name: 'manhattan', args: { startDirections: ['left','right'],endDirections: ['left','right'] } },
@@ -91,35 +152,27 @@ Template.pipecanvas.onRendered(function() {
         },
         markAvailable: true
     });
+    self.papers.push(paper);
 
-    self.paper.options.defaultConnector = {
+    self.papers[index].options.defaultConnector = {
         name: 'rounded',
         args: {
             radius: 10
         }
     }
 
-    self.paper.on('cell:pointerdblclick',
+    self.papers[index].on('cell:pointerdblclick',
         function(cellView, evt, x, y) {
             cellView.model.remove();
             self.update();
         }
     );
 
-    self.graph.on('change', function() {
+    self.graphs[index].on('change', function() {
         self.update();
     });
-
-    Tracker.autorun(function() {
-        let contract_function = self.functionAbi.get();
-        if (contract_function) {
-            addNode(self.graph, self.nodes, contract_function)
-        }
-    });
-
-});
-
-var gr, sol, run, cells, funcs=[];
+    console.log(self);
+}
 
 function addNode(graph, nodes, contract_function) {
     // console.log('addNode', contract_function)
@@ -176,259 +229,4 @@ function addNode(graph, nodes, contract_function) {
         }).addTo(graph)
     )
     return nodes
-}
-
-function toSolidity(graph){
-    preRun(graph)
-
-    gr = graph.toJSON();
-
-    sol = solidity.file_p0;
-    sol = sol + solidity.import_p0+ solidity.import_p1;
-    sol = sol + solidity.proxy;
-    sol = sol + solidity.contract_p0;
-    sol = sol + 'PipedContract';
-
-    sol = sol + solidity.contract_p1;
-    let contrs =[], ffunc=[], fffunc=[], tm=""
-    for (let nx in funcs){
-        sol = sol + "address public "+funcs[nx].attrs[".label"].text.split(".\n")[0]+"_address;\n";
-        ffunc.push("address _"+funcs[nx].attrs[".label"].text.split(".\n")[0]+"_address")
-        tm = funcs[nx].attrs[".label"].text.split(".\n")[0]+"_address"
-        fffunc.push(tm+" = _"+tm+";\n")
-    }
-
-    sol = sol + "\nconstructor( address _seth_proxy, "+ffunc.join(",")+") public {\nseth_proxy = SethProxy(_seth_proxy);\n";
-    sol = sol + fffunc.join("");
-    sol = sol + "\n}\n"
-
-    sol = sol + solidity.function_p0;
-    sol = sol + 'PipedFunction';
-    sol = sol + solidity.function_pp0;
-    // console.log(inputs)
-    inputs.forEach((inP, ndx) => {
-        if (inP.split(".")[1] == "uint256: wei_value") return;
-        if (inP.split(".")[1] == "address: tx_sender") return;
-        sol = sol+inP.split(".")[1].replace(": "," ")+", "
-    })
-    sol = sol.substring(0, sol.length - 2);
-
-    sol = sol + solidity.function_pp1;
-    sol = sol + solidity.function_p1;
-    if (run.outPorts.length > 0) {
-        sol = sol + solidity.function_ret0;
-        run.outPorts.forEach((inP, ndx) => {
-            sol = sol+inP.split(".")[1].replace(": "," ")+", "
-        })
-        sol = sol.substring(0, sol.length - 2);
-        sol = sol + solidity.function_ret1
-    }
-
-    sol = sol + solidity.function_p2;
-
-    let inArgs = "", outArgs="";
-    for (let nx in funcs){
-        forFunc(funcs[funcs.length-nx-1])
-    }
-
-    if (run.outPorts.length > 0) {
-        sol = sol + "return ("
-        run.outPorts.forEach((inP, ndx) => {
-            sol = sol+inP.split(".")[1].replace(": "," ")+", "
-        })
-        sol = sol.substring(0, sol.length - 2)+");\n";
-    }
-
-    sol = sol + solidity.function_p3;
-    sol = sol + solidity.contract_p2;
-    return sol;
-    // $('#source_textarea').val(sol);
-}
-
-function forFunc(func){
-    // console.log(func)
-        let outs = [], ins = [], touts = [], tins = [], tins2=[], ttins=[]
-        func.outPorts.forEach((out, dx)=>{
-            outs.push(getIO(func.id, out, 1))
-            touts.push(out)
-        })
-        func.inPorts.forEach((inn, dx)=>{
-            let inIt = getIO(func.id, inn, 0);
-            if (!inIt) {
-                ins.push(inn.split(" ")[1])
-            } else {
-                ins.push(inIt)
-            }
-            tins.push(inn.split(": ")[0])
-            tins2.push(getIO2(func.id, inn, 0))
-            ttins.push(inn.split(": ")[1])
-
-        })
-        if (outs.length >0 ) {
-            if (!outs[0]) {
-                //sol = sol + "("+run.outPorts.join(", ").replace(": "," ")+") = ";
-            } else {
-                //sol = sol + "("+outs.join(", ").replace(": "," ")+") = ";
-            }
-
-        }
-        // console.log(ins)
-        sol = sol + "\nsignature42 = bytes4(keccak256(\""+func.attrs[".label"].text.split(".\n")[1]+"("+tins.join(",")+")\"));\ninput42 = abi.encodeWithSelector(signature42, "+tins2.join(",")+");\n"
-        if (touts.length > 0){
-            sol = sol + "answer42 = ";
-        }
-        sol = sol + "seth_proxy.proxyCallInternal";
-        if (func.attrs["rect"].fill == "#edd") {
-            sol = sol + ".value(msg.value)";
-        }
-        sol = sol +"("+func.attrs[".label"].text.split(".\n")[0]+"_address , input42, 32);\n"
-        touts.forEach((o,n)=>{
-            sol = sol +o.split(": ").join(" ")+";\n"
-        })
-        if (touts.length > 0){
-            sol = sol +"assembly {\n"
-            touts.forEach((o,n)=>{
-                sol = sol +o.split(" ")[1]+" := mload(add(answer42, 32))\n"
-            })
-            sol = sol +"}\n"
-        }
-
-
-
-
-}
-
-var gr, sol, run, cells, funcs=[];
-
-function getNext(edge, port){
-    let next = {}
-    // console.log(cells)
-    for (let i in cells){
-        if (cells[i].type == "link") {
-            if (edge == cells[i].source.id){
-                next[cells[i].source.port] = cells[i].target
-            }
-        }
-
-    }
-    return next
-}
-
-function getIO(node, port, io){
-    let dir = "target", extra = "";
-    if (io === 1) {
-        dir = "source"
-    }
-    for (let i in cells){
-        if (cells[i].type == "link") {
-            if (node == cells[i][dir].id && port == cells[i][dir].port){
-                //next[cells[i].source.port] = cells[i].target
-                //console.log(cells[i].id)
-                if (io === 1){
-                    extra = port.split(": ")[0]
-                }
-                return (extra+" e_"+cells[i].source.id+cells[i].source.port).replace(/: /g,"")
-            }
-        }
-    }
-    return false;
-}
-
-function getIO2(node, port, io){
-    let dir = "target", extra = "";
-    if (io === 1) {
-        dir = "source"
-    }
-    for (let i in cells){
-        if (cells[i].type == "link") {
-            if (node == cells[i][dir].id && port == cells[i][dir].port){
-                //next[cells[i].source.port] = cells[i].target
-                //console.log(cells[i].id)
-                if (io === 1){
-                    extra = port.split(": ")[0]
-                }
-                return cells[i].source.port.split(": ")[1]
-            }
-        }
-    }
-
-    for (let ndx in run.inPorts){
-        if (run.inPorts[ndx].split(".")[1] == port) {
-            // console.log("hi hi ",port.split(": ")[1])
-            return port.split(": ")[1]
-        }
-    }
-
-    return false;
-}
-
-
-function preRun(graph) {
-    run={inPorts:[], outPorts:[], toRun:[], ready:[]}
-    proc ={}
-    funcs = []
-    cells = graph.toJSON().cells;
-    for (let i in cells){
-        if (cells[i].type == "devs.Atomic" && cells[i].attrs.pipeline.abi.type != 'event') {
-            //run.inPorts.push()
-            cells[i].inPorts.forEach((inP, index) => {
-                run.inPorts.push(cells[i].id+"."+inP)
-            })
-            cells[i].outPorts.forEach((inP, index) => {
-                run.outPorts.push(cells[i].id+"."+inP)
-            })
-            run.toRun.push(cells[i])
-        }
-    }
-
-
-
-    cells.forEach((cell, index) => {
-        if (cell.type == "link") {
-            //console.log(cell.source.id+"."+cell.source.port)
-            run.outPorts.splice(run.outPorts.indexOf(cell.source.id+"."+cell.source.port), 1);
-            run.inPorts.splice(run.inPorts.indexOf(cell.target.id+"."+cell.target.port), 1);
-        }
-    })
-    inputs = $.extend(true, [], run.inPorts);
-    process(run)
-}
-
-function process(proc){
-    proc.ready =[]
-
-    proc.toRun.forEach((cell, index) => {
-        proc.ready.push(cell);
-        cell.outPorts.forEach((port,ndx)=> {
-            if (run.outPorts.indexOf(cell.id+"."+port ) <0 ){
-                //console.log((cell.id+"."+port))
-                proc.ready.pop();
-            }
-        })
-
-    })
-    //console.log(proc)
-    if (proc.ready.length == 0) return;
-    // console.log(proc.ready)
-
-    proc.ready.forEach((cell, index) => {
-
-        proc.toRun.splice(proc.toRun.indexOf(cell), 1)
-        proc.outPorts = []
-        proc.toRun.forEach((rcell, ndx)=> {
-            if (rcell.type == "devs.Atomic") {
-                //run.inPorts.push()
-                rcell.outPorts.forEach((inP, index2) => {
-                    proc.outPorts.push(rcell.id+"."+inP)
-                })
-            }
-        })
-        runFunc(cell)
-    })
-    process(proc)
-}
-
-function runFunc(arg){
-    //console.log(arg)
-    funcs.push(arg)
 }
---
+++ imports/client/templates/pipecanvas/toSolidity.js
@@ -0,0 +1,312 @@
+import solidity from '../pipecodepreview/codepatterns.js';
+
+export default class GraphsToSolidity {
+    constructor(graphs) {
+        this.solidity_code = '';
+        this.graphs = graphs.map(function(graph, i) {
+            return new GraphToSolidity(graph, i + 1);
+        });
+        this.ffunc = [];
+        this.fffunc = [];
+        this.contract_names = [];
+    }
+
+    toSolidity() {
+        let self = this;
+        this.solidity_code = solidity.file_p0;
+        // this.solidity_code += solidity.import_p0 + solidity.import_p1;
+        this.solidity_code += solidity.proxy;
+        this.solidity_code += solidity.contract_p0;
+        this.solidity_code += 'PipedContract';
+
+        this.solidity_code += solidity.contract_p1;
+
+        this.preRun();
+        this.setContractAddresses();
+        this.setConstructor();
+
+        this.graphs.forEach(function(graph) {
+            self.solidity_code += graph.toSolidity();
+        });
+        this.solidity_code += solidity.contract_p2;
+
+        return this.solidity_code
+    }
+
+    preRun() {
+        this.graphs.forEach(function(graph) {
+            graph.preRun();
+            // console.log('preRun', graph.funcs);
+        });
+    }
+
+    getContractAddresses(graph) {
+        let tm = '', code = '';
+        for (let nx in graph.funcs){
+            let contract_name = graph.funcs[nx].attrs[".label"].text.split(".\n")[0];
+
+            if (this.contract_names.indexOf(contract_name) == -1) {
+                this.contract_names.push(contract_name);
+                code += "    address public " + contract_name + "_address;\n";
+                this.ffunc.push("address _" + contract_name + "_address")
+                tm = contract_name + "_address"
+                this.fffunc.push(tm + " = _" + tm + ";\n    ")
+            }
+        }
+        return code;
+    }
+
+    setContractAddresses() {
+        let self = this;
+        this.graphs.forEach(function(graph) {
+            // console.log('setContractAddresses', graph)
+            self.solidity_code += self.getContractAddresses(graph);
+        });
+    }
+
+    setConstructor() {
+        let code = '';
+        code += "\n    constructor( address _seth_proxy, " + this.ffunc.join(", ") + ") public {\n        seth_proxy = SethProxy(_seth_proxy);\n    ";
+        code += this.fffunc.join("");
+        code += "\n}\n";
+        this.solidity_code += code;
+    }
+}
+
+class GraphToSolidity {
+    constructor(graph, index) {
+        this.graph = graph;
+        this.index = index;
+        this.solidity_code = '';
+        this.run = {inPorts:[], outPorts:[], toRun:[], ready:[]};
+        this.cells = [];
+        this.funcs = [];
+        this.inputs = [];
+    }
+
+    toSolidity(){
+        // this.preRun();
+
+        this.solidity_code += solidity.function_p0;
+        this.solidity_code += 'PipedFunction' + this.index;
+        this.solidity_code += solidity.function_pp0;
+        // console.log(inputs)
+        this.inputs.forEach((inP, ndx) => {
+            if (inP.split(".")[1] == "uint256: wei_value") return;
+            if (inP.split(".")[1] == "address: tx_sender") return;
+            this.solidity_code += inP.split(".")[1].replace(": "," ")+", "
+        })
+        this.solidity_code = this.solidity_code.substring(0, this.solidity_code.length - 2);
+
+        this.solidity_code += solidity.function_pp1;
+        this.solidity_code += solidity.function_p1;
+        if (this.run.outPorts.length > 0) {
+            this.solidity_code += solidity.function_ret0;
+            this.run.outPorts.forEach((inP, ndx) => {
+                this.solidity_code += inP.split(".")[1].replace(": "," ")+", "
+            })
+            this.solidity_code = this.solidity_code.substring(0, this.solidity_code.length - 2);
+            this.solidity_code += solidity.function_ret1
+        }
+
+        this.solidity_code += solidity.function_p2;
+
+        let inArgs = "", outArgs="";
+        for (let nx in this.funcs){
+            this.forFunc(this.funcs[this.funcs.length-nx-1])
+        }
+
+        if (this.run.outPorts.length > 0) {
+            this.solidity_code += "return ("
+            this.run.outPorts.forEach((inP, ndx) => {
+                this.solidity_code += inP.split(".")[1].replace(": "," ")+", "
+            })
+            this.solidity_code = this.solidity_code.substring(0, this.solidity_code.length - 2)+");\n";
+        }
+
+        this.solidity_code += solidity.function_p3;
+        return this.solidity_code;
+        // $('#source_textarea').val(sol);
+    }
+
+    forFunc(func){
+        // console.log(func)
+            let outs = [], ins = [], touts = [], tins = [], tins2=[], ttins=[]
+            func.outPorts.forEach((out, dx)=>{
+                outs.push(this.getIO(func.id, out, 1))
+                touts.push(out)
+            })
+            func.inPorts.forEach((inn, dx)=>{
+                let inIt = this.getIO(func.id, inn, 0);
+                if (!inIt) {
+                    ins.push(inn.split(" ")[1])
+                } else {
+                    ins.push(inIt)
+                }
+                tins.push(inn.split(": ")[0])
+                tins2.push(this.getIO2(func.id, inn, 0))
+                ttins.push(inn.split(": ")[1])
+
+            })
+            if (outs.length >0 ) {
+                if (!outs[0]) {
+                    //sol = sol + "("+this.run.outPorts.join(", ").replace(": "," ")+") = ";
+                } else {
+                    //sol = sol + "("+outs.join(", ").replace(": "," ")+") = ";
+                }
+
+            }
+            // console.log(ins)
+            this.solidity_code += "\n    signature42 = bytes4(keccak256(\""+func.attrs[".label"].text.split(".\n")[1]+"("+tins.join(",")+")\"));\n    input42 = abi.encodeWithSelector(signature42, "+tins2.join(",")+");\n"
+            if (touts.length > 0){
+                this.solidity_code += "    answer42 = ";
+            }
+            this.solidity_code += "    seth_proxy.proxyCallInternal";
+            if (func.attrs["rect"].fill == "#edd") {
+                this.solidity_code += ".value(msg.value)";
+            }
+            this.solidity_code += "("+func.attrs[".label"].text.split(".\n")[0]+"_address , input42, 32);\n"
+            touts.forEach((o,n)=>{
+                this.solidity_code += '    ' + o.split(": ").join(" ")+";\n"
+            })
+            if (touts.length > 0){
+                this.solidity_code += "    assembly {\n"
+                touts.forEach((o,n)=>{
+                    this.solidity_code += '    ' + o.split(" ")[1]+" := mload(add(answer42, 32))\n"
+                })
+                this.solidity_code += "    }\n"
+            }
+
+    }
+
+    /*getNext(edge, port){
+        let next = {}
+        // console.log(this.cells)
+        for (let i in this.cells){
+            if (this.cells[i].type == "link") {
+                if (edge == this.cells[i].source.id){
+                    next[this.cells[i].source.port] = this.cells[i].target
+                }
+            }
+
+        }
+        return next
+    }*/
+
+    getIO(node, port, io){
+        let dir = "target", extra = "";
+        if (io === 1) {
+            dir = "source"
+        }
+        for (let i in this.cells){
+            if (this.cells[i].type == "link") {
+                if (node == this.cells[i][dir].id && port == this.cells[i][dir].port){
+                    //next[this.cells[i].source.port] = this.cells[i].target
+                    //console.log(this.cells[i].id)
+                    if (io === 1){
+                        extra = port.split(": ")[0]
+                    }
+                    return (extra+" e_"+this.cells[i].source.id+this.cells[i].source.port).replace(/: /g,"")
+                }
+            }
+        }
+        return false;
+    }
+
+    getIO2(node, port, io){
+        let dir = "target", extra = "";
+        if (io === 1) {
+            dir = "source"
+        }
+        for (let i in this.cells){
+            if (this.cells[i].type == "link") {
+                if (node == this.cells[i][dir].id && port == this.cells[i][dir].port){
+                    //next[this.cells[i].source.port] = this.cells[i].target
+                    //console.log(this.cells[i].id)
+                    if (io === 1){
+                        extra = port.split(": ")[0]
+                    }
+                    return this.cells[i].source.port.split(": ")[1]
+                }
+            }
+        }
+
+        for (let ndx in this.run.inPorts){
+            if (this.run.inPorts[ndx].split(".")[1] == port) {
+                // console.log("hi hi ",port.split(": ")[1])
+                return port.split(": ")[1]
+            }
+        }
+
+        return false;
+    }
+
+    preRun() {
+        let proc ={}
+        this.funcs = []
+        this.cells = this.graph.toJSON().cells;
+        for (let i in this.cells){
+            if (this.cells[i].type == "devs.Atomic" && this.cells[i].attrs.pipeline.abi.type != 'event') {
+                //this.run.inPorts.push()
+                this.cells[i].inPorts.forEach((inP, index) => {
+                    this.run.inPorts.push(this.cells[i].id+"."+inP)
+                })
+                this.cells[i].outPorts.forEach((inP, index) => {
+                    this.run.outPorts.push(this.cells[i].id+"."+inP)
+                })
+                this.run.toRun.push(this.cells[i])
+            }
+        }
+
+
+
+        this.cells.forEach((cell, index) => {
+            if (cell.type == "link") {
+                //console.log(cell.source.id+"."+cell.source.port)
+                this.run.outPorts.splice(this.run.outPorts.indexOf(cell.source.id+"."+cell.source.port), 1);
+                this.run.inPorts.splice(this.run.inPorts.indexOf(cell.target.id+"."+cell.target.port), 1);
+            }
+        })
+        this.inputs = $.extend(true, [], this.run.inPorts);
+        this.process(this.run)
+    }
+
+    process(proc){
+        proc.ready =[]
+
+        proc.toRun.forEach((cell, index) => {
+            proc.ready.push(cell);
+            cell.outPorts.forEach((port,ndx)=> {
+                if (this.run.outPorts.indexOf(cell.id+"."+port ) <0 ){
+                    //console.log((cell.id+"."+port))
+                    proc.ready.pop();
+                }
+            })
+
+        })
+        //console.log(proc)
+        if (proc.ready.length == 0) return;
+        // console.log(proc.ready)
+
+        proc.ready.forEach((cell, index) => {
+
+            proc.toRun.splice(proc.toRun.indexOf(cell), 1)
+            proc.outPorts = []
+            proc.toRun.forEach((rcell, ndx)=> {
+                if (rcell.type == "devs.Atomic") {
+                    //this.run.inPorts.push()
+                    rcell.outPorts.forEach((inP, index2) => {
+                        proc.outPorts.push(rcell.id+"."+inP)
+                    })
+                }
+            })
+            this.runFunc(cell)
+        })
+        this.process(proc)
+    }
+
+    runFunc(arg){
+        //console.log(arg)
+        this.funcs.push(arg)
+    }
+}
